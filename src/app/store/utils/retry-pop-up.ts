152c34b3e0055f4b5f64796efd8c7c0d5fe84df3
Adrian Czaicki <adrianrobimuzyke@gmail.com>
2022-08-10 11:06:32 +0200

    fix config logic

--- src/app/store/utils/retry-pop-up.ts
+++ src/app/store/utils/retry-pop-up.ts
@@ -3,12 +3,15 @@ import {catchError, iif, pipe, retry, switchMap, throwError} from "rxjs";
 import {MatSnackBar, MatSnackBarConfig} from "@angular/material/snack-bar";
 
 
-export function retryPopUp(injector: Injector, config: MatSnackBarConfig = {duration: 5000, verticalPosition: "top"}) {
+export function retryPopUp(injector: Injector, config?: MatSnackBarConfig) {
   return pipe(
     catchError((err, caught) => {
 
       const snackBar = injector.get(MatSnackBar)
-      const snackBarRef = snackBar.open('Connection failure', 'Retry', config)
+      const snackBarRef = snackBar.open('Connection failure', 'Retry', {
+        duration: 5000,
+        verticalPosition: "top", ...config
+      })
 
       return snackBarRef.afterDismissed()
         .pipe(
